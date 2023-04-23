15a1bab5e9d81261325da12c96cb3220320de662
Denys <denys.drhn@gmail.com>
2023-04-23 23:11:01 +0300

    Create ImageGalleryItem.jsx

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './ImageGalleryItem.module.css';
+import Modal from 'components/Modal/Modal';
+
+export default class ImageGalleryItem extends Component {
+  static propTypes = {
+    image: PropTypes.shape({
+      webformatURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+    }).isRequired,
+    index: PropTypes.number.isRequired,
+  };
+
+  state = {
+    isModalOpen: false,
+    largeImageURL: '',
+  };
+
+  toggleModal = () => {
+    this.setState(({ isModalOpen }) => ({
+      isModalOpen: !isModalOpen,
+    }));
+  };
+
+  handleOpenModal = largeImageURL => {
+    this.setState({
+      isModalOpen: true,
+      largeImageURL,
+    });
+  };
+
+  handleCloseModal = () => {
+    this.setState({
+      isModalOpen: false,
+    });
+  };
+
+  render() {
+    const { image, index } = this.props;
+    const { isModalOpen } = this.state;
+    return (
+      <li key={index} className={css.ImageGalleryItem}>
+        <img
+          className={css.ImageGalleryItemImage}
+          src={image.webformatURL}
+          alt={image.tags}
+          onClick={() => this.handleOpenModal(image.largeImageURL)}
+        />
+        {isModalOpen && <Modal onClose={this.handleCloseModal} image={image} />}
+      </li>
+    );
+  }
+}
