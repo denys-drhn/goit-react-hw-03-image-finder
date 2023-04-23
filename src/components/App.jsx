deb9926698427b124e06f8615caf93ecadf6b484
Denys <denys.drhn@gmail.com>
2023-04-23 23:10:41 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Searchbar from './Searchbar/Searchbar';
+
+export class App extends Component {
+  state = {
+    imageName: '',
+  };
+
+  handleFormSubmit = imageName => {
+    this.setState({ imageName });
+  };
+
+  render() {
+    const { imageName } = this.state;
+    return (
+      <>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery imageName={imageName} />
+      </>
+    );
+  }
+}
