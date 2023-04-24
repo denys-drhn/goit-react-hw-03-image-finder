d03f0295629e550a90a80c98b87a20749da650d3
Denys <denys.drhn@gmail.com>
2023-04-25 00:36:40 +0300

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,16 @@
 import css from './Button.module.css';
 import PropTypes from 'prop-types';
 
-export default function Button({ onClick, children }) {
+const Button = ({ onClick }) => {
   return (
     <button className={css.Button} type="button" onClick={onClick}>
-      {children}
+      Load More
     </button>
   );
-}
+};
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired,
 };
+
+export default Button;
