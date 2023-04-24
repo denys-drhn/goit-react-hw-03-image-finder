2943979edca1a0a3e9a401e0acfa5046557532b6
Denys <denys.drhn@gmail.com>
2023-04-24 23:59:21 +0300

    Update ImagesApiServise.js

--- src/services/ImagesApiServise.js
+++ src/services/ImagesApiServise.js
@@ -1,38 +1,13 @@
 import axios from 'axios';
-export default class ImagesApiServise {
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-  }
 
-  async fechImages() {
-    const params = {
-      key: '34146115-f93b131a505bf9d05e96b838b',
-      q: this.searchQuery,
-      page: this.page,
-      per_page: 12,
-      orientation: 'horizontal',
-      safesearch: true,
-      image_type: 'photo',
-    };
-    const response = await axios.get('https://pixabay.com/api/', { params });
-    // console.log(response);
-    const data = response.data;
-    // console.log(response.data);
-    // console.log(data);
-    this.page += 1;
-    return data;
-  }
+axios.defaults.baseURL =
+  'https://pixabay.com/api/?key=34146115-f93b131a505bf9d05e96b838b&image_type=photo&orientation=horizontal';
 
-  resetPage() {
-    this.page = 1;
+export const getImages = async (query, page) => {
+  try {
+    const response = await axios.get(`&per_page=12&page=${page}&q=${query}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
   }
-
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
-}
+};
