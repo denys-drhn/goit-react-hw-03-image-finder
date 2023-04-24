94c6bf2e13255f2e0cb2c9ecef9519350d67ba0c
Denys <denys.drhn@gmail.com>
2023-04-25 00:36:51 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,10 @@
 import { InfinitySpin } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
-const Spinner = () => (
-  <div className={css.Spinner}>
+const Loader = () => (
+  <div className={css.Loader}>
     <InfinitySpin width="200" color="#4fa94d" />
   </div>
 );
 
-export default Spinner;
+export default Loader;
